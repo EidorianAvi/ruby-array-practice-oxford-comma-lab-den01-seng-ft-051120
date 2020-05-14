7752d519e902aabdfea46cfa8e52cfc7acac8ef9
Rene Avila <reneavila1993@gmail.com>
2020-05-14 19:19:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2 
     return array.join(" and ")
   else 
-    return array[0..1].join(", ") + ', and ' + array[2]
+    return array[0..-2].join(", ") + ', and ' + array.last
   end
 end
